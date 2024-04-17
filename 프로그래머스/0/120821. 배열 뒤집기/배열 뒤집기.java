3e0bd751a0488f5a2e78996a6cf3ced7d68de9a9
eunoia73 <158018280+eunoia73@users.noreply.github.com>
2024-04-17 17:50:23 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        int a = num_list.length-1;
+        for(int i=0; i<num_list.length; i++){
+            answer[i] = num_list[a];
+            a = a-1;
+        }
+        
+        return answer;
+    }
+}
