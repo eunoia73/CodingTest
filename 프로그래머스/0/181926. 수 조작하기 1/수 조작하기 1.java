7829cb1d27fe164d064080325ea1e0d8b47ee02e
eunoia73 <158018280+eunoia73@users.noreply.github.com>
2024-05-17 14:33:11 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.63 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = n;
+        char[] arr = control.toCharArray();
+        
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == 'w') answer += 1;
+            else if(arr[i] == 's') answer -= 1;
+            else if(arr[i] == 'd') answer += 10; 
+            else answer -= 10;
+        }
+       
+        return answer;
+    }
+}
