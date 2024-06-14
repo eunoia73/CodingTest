c487a3b0fbe431e897a6c61cd81083b6bb4a9781
eunoia73 <158018280+eunoia73@users.noreply.github.com>
2024-06-14 09:54:51 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.01 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length];
+        int num = 0;
+        //n부터 끝까지
+        for(int i = n; i< num_list.length; i++){
+            answer[num] = num_list[i];
+            num++;
+        }
+        
+        //맨앞에서 n까지
+        int num2 = num_list.length - n;
+        for(int i = 0; i < n; i++){
+            answer[num2] = num_list[i];
+            num2++;
+        }
+        
+        return answer;
+    }
+}
