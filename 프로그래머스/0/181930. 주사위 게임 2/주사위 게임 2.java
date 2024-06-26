2d35e0f04a0b28286c8fdbb08a22c6399dd92405
eunoia73 <158018280+eunoia73@users.noreply.github.com>
2024-06-26 23:52:18 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.02 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int a, int b, int c) {
         int answer = 0;
+        
         //모두 같다면
         if(a == b && b == c){
             answer = (a+b+c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
