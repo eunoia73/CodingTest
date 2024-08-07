40807bc0d5a6576f60964a8d420ce63dd932a555
eunoia73 <158018280+eunoia73@users.noreply.github.com>
2024-08-07 11:28:39 +0900

    [Bronze II] Title: 단어의 개수, Time: 436 ms, Memory: 38632 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+         if (s.equals(" ")) {
+            System.out.println(0);
+        }else{
+            String trimmed = s.trim();
+            String[] splited = trimmed.split(" ");
+            System.out.println(splited.length);
+        }
+    }
+}
