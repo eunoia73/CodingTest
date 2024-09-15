778467e3520a5359b4dfe3584a9bf8889b15c1e2
eunoia73 <158018280+eunoia73@users.noreply.github.com>
2024-09-15 16:19:35 +0900

    [Bronze V] Title: 윤년, Time: 168 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 윤년
+        // 윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(1);
+        } else System.out.println(0);
+    }
+}
