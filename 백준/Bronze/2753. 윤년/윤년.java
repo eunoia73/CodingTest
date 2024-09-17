59d9f6104c55b273e7875f6bc51eb2e7489348d3
eunoia73 <158018280+eunoia73@users.noreply.github.com>
2024-09-17 14:26:52 +0900

    윤년.java 업데이트

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 윤년
+        // 윤년 
         // 윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
         Scanner scanner = new Scanner(System.in);
         int year = scanner.nextInt();
