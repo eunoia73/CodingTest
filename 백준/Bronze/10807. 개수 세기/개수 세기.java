5a61160d5798d0ab8cc90137a17c54df0f8018f7
eunoia73 <158018280+eunoia73@users.noreply.github.com>
2024-09-21 13:57:36 +0900

    [Bronze V] Title: 개수 세기, Time: 176 ms, Memory: 17816 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 개수 세기
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int num = scanner.nextInt();
+
+        int answer = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (num == arr[i]) answer++;
+        }
+        System.out.println(answer);
+
+
+    }
+}
