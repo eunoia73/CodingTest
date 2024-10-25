cd5d07e8c585c74145a6a8d401ea7c6d9b6f48ec
eunoia73 <158018280+eunoia73@users.noreply.github.com>
2024-10-25 10:54:14 +0900

    [Bronze II] Title: 공 바꾸기, Time: 208 ms, Memory: 18536 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //공 바꾸기
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();  //바구니
+        int b = scanner.nextInt();  //횟수
+        int[] arr = new int[a];
+        for (int i = 0; i < a; i++) {
+            arr[i] = i + 1;
+        }
+
+        for (int i = 0; i < b; i++) {
+            int q = scanner.nextInt();
+            int w = scanner.nextInt();
+            int temp = arr[q - 1];
+            arr[q - 1] = arr[w - 1];
+            arr[w - 1] = temp;
+
+        }
+
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
