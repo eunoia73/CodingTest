fe16fe0702731318668891c9e297896a1b6bbe50
eunoia73 <158018280+eunoia73@users.noreply.github.com>
2024-11-01 21:36:45 +0900

    개.java 업데이트

--- 백준/Bronze/10172. 개/개.java
+++ 백준/Bronze/10172. 개/개.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("|\\_/|\n");
+        System.out.print("|\\_/|\n"); 
         System.out.print("|q p|   /}\n");
         System.out.print("( 0 )\"\"\"\\\n");
         System.out.print("|\"^\"`    |\n");
