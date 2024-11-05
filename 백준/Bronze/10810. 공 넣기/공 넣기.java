5e7865d9c632ab05b03ee1531dbe4afef4368a28
eunoia73 <158018280+eunoia73@users.noreply.github.com>
2024-11-05 13:33:42 +0900

    [Bronze III] Title: 공 넣기, Time: 120 ms, Memory: 15920 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 공 넣기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine(), " ");
+        int length = Integer.parseInt(st.nextToken());  //바구니개수
+        int n = Integer.parseInt(st.nextToken());  //명령개수
+
+        int[] arr = new int[length];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            for (int j = a - 1; j <= b - 1; j++) {
+                arr[j] = c;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            bw.write(arr[i] + " ");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
