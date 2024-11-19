1ee70262b45b63333fdc13cf3f9409fb2a3824d5
eunoia73 <158018280+eunoia73@users.noreply.github.com>
2024-11-19 17:30:38 +0900

    [Bronze V] Title: A+B - 4, Time: 188 ms, Memory: 17824 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    //EOF end of file
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()){
+            String s = scanner.nextLine();
+            String[] split = s.split(" ");
+            int a = Integer.parseInt(split[0]);
+            int b = Integer.parseInt(split[1]);
+            System.out.println(a + b);
+        }
+
+    }
+}
